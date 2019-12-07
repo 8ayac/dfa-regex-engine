38f1926d75143546a3c157bd4433d33c44f41206
8ayac <8ayac.y@gmail.com>
2019-12-08 03:28:17 +0900

    Merge *Lexer.getToken() to *Lexer.Scan().

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -19,31 +19,26 @@ func NewLexer(s string) *Lexer {
 	}
 }
 
-// getToken return the appropriate token for the given symbol.
-func (l *Lexer) getToken(r rune) *token.Token {
-	switch r {
-	case '\x00':
-		return token.NewToken(r, token.EOF)
-	case '\\':
-		return token.NewToken(r, token.CHARACTER)
-	case '|':
-		return token.NewToken(r, token.UNION)
-	case '(':
-		return token.NewToken(r, token.LPAREN)
-	case ')':
-		return token.NewToken(r, token.RPAREN)
-	case '*':
-		return token.NewToken(r, token.STAR)
-	default:
-		return token.NewToken(r, token.CHARACTER)
-	}
-}
-
-// Scan returns the all token to which converted from
+// Scan returns the token list to which converted from
 // the symbol slice held in Lexer struct.
 func (l *Lexer) Scan() (tokenList []*token.Token) {
 	for i := 0; i < len(l.s); i++ {
-		tokenList = append(tokenList, l.getToken(l.s[i]))
+		switch l.s[i] {
+		case '\x00':
+			tokenList = append(tokenList, token.NewToken(l.s[i], token.EOF))
+		case '\\':
+			tokenList = append(tokenList, token.NewToken(l.s[i], token.CHARACTER))
+		case '|':
+			tokenList = append(tokenList, token.NewToken(l.s[i], token.UNION))
+		case '(':
+			tokenList = append(tokenList, token.NewToken(l.s[i], token.LPAREN))
+		case ')':
+			tokenList = append(tokenList, token.NewToken(l.s[i], token.RPAREN))
+		case '*':
+			tokenList = append(tokenList, token.NewToken(l.s[i], token.STAR))
+		default:
+			tokenList = append(tokenList, token.NewToken(l.s[i], token.CHARACTER))
+		}
 	}
 	return
 }
